ee78976d3dfc0dbf3af542651b29e079f6628a92
Your Name <kulyash.orazbekova@ce.sdu.edu.kz>
2015-03-12 18:10:20 +0600

    all task 59%

--- test/test_xml2json.py
+++ test/test_xml2json.py
@@ -43,5 +43,53 @@ class SimplisticTest(unittest.TestCase):
         self.assertTrue("table" in json_data["root"])
         self.assertEqual(json_data["root"]["table"][0]["tr"]["td"] , ["Apples", "Bananas"])
 
+    def test_json2xml(self):
+        json_data = '{"e": { "@name": "value"}}' #3
+        xml_string = xml2json.json2xml(json_data)
+        self.assertEqual(xml_string, '<e name="value" />')
+
+        json_data = '{"e": null}' #1
+        xml_string = xml2json.json2xml(json_data)
+        self.assertEqual(xml_string, '<e />')
+
+        json_data = '{"e": { "#text": "text", "a": "text"}}' #7
+        xml_string = xml2json.json2xml(json_data)
+        self.assertEqual(xml_string, '<e>text<a>text</a></e>')
+
+        json_data = '{"e": { "a": ["text", "text"]}}' #6
+        xml_string = xml2json.json2xml(json_data)
+        self.assertEqual(xml_string, '<e><a>text</a><a>text</a></e>')
+
+        json_data = '{"e": { "a": "text", "b": "text"}}' #5
+        xml_string = xml2json.json2xml(json_data)
+        self.assertEqual(xml_string, '<e><a>text</a><b>text</b></e>')
+
+    def test_json2xml3(self):
+        json_data = '{"e": { "@name": "value", "#text": "text" }}' #4
+        xml_string = xml2json.internal_to_elem
+        #self.assertEqual(xml_string, '<e name="value">text</e>')
+
+
+    def test_json2xml2(self):
+
+        json_data = '{"e": null}' 
+        xml_string = xml2json.json2elem(json_data)
+        self.assertEqual(json_data, xml2json.elem2json(xml_string,options))
+
+        json_data = '{"e": {"a": "text", "b": "text"}}' 
+        xml_string = xml2json.json2elem(json_data)
+        self.assertEqual(json_data, xml2json.elem2json(xml_string,options))
+
+        json_data = '{"e": {"@": "1", "#tail": "2", "#text": "3", "q":["dnja","lknvdsl"]}}' #4
+        xml_string = xml2json.json2xml(json_data)
+        # self.assertEqual(xml_string, '<e @="1">tail</e>')
+
+        json_data = '{"e": "text"}' #2
+        xml_string = xml2json.json2xml(json_data)
+        self.assertEqual(xml_string, '<e>text</e>')
+
+        json_data = '{"e":"a","r":"d"}'
+        with self.assertRaises(ValueError) as cm: xml2json.json2xml(json_data)
+
 if __name__ == '__main__':
     unittest.main()
